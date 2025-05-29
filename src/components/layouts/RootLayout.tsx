97a084774302b6530a42857df57c679ed2970ef7
Mayur-505 <mayurbhuva505@gmail.com>
2025-05-29 10:46:30 +0530

    setup: react query

--- src/components/layouts/RootLayout.tsx
+++ src/components/layouts/RootLayout.tsx
@@ -1,9 +1,11 @@
-import { Outlet } from "react-router";
+import { cn } from "@/lib/utils";
+import { Outlet, useLocation } from "react-router";
 import Header from "./Header";
 
 const RootLayout = () => {
+	const location = useLocation();
 	return (
-		<div className="min-h-screen overflow-auto">
+		<div className={cn("bg-grey min-h-screen overflow-auto pb-10", location.pathname === "/" && "bg-white")}>
 			<Header />
 			<Outlet />
 		</div>
